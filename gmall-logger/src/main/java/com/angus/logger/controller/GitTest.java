2bb0909e8b757d6e1808ccdd342e9764548dc360
Angus <15547477278@163.com>
2022-02-22 14:48:17 +0800

    second commit

--- gmall-logger/src/main/java/com/angus/logger/controller/GitTest.java
+++ gmall-logger/src/main/java/com/angus/logger/controller/GitTest.java
@@ -7,6 +7,6 @@ package com.angus.logger.controller;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        System.out.println("Hello Git2");
     }
 }
