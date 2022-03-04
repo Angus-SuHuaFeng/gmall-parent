4f92eecb9f88d416908a9b3667284e87065e89f7
Angus <15547477278@163.com>
2022-03-04 23:14:28 +0800

    Angus

--- gmall-realtime/src/main/scala/dws/OrderWideApp.scala
+++ gmall-realtime/src/main/scala/dws/OrderWideApp.scala
@@ -79,8 +79,7 @@ object OrderWideApp {
     // 开窗
     val orderInfoWindowDStream: DStream[(Long, OrderInfo)] = orderInfoDStream.window(Seconds(20), Seconds(8))
     val orderDetailWindowDStream: DStream[(Long, OrderDetail)] = orderDetailDStream.window(Seconds(20), Seconds(8))
-
-    // 双流Join
+    // =======================2.双流Join=========================
     val joinedDStream: DStream[(Long, (OrderInfo, OrderDetail))] = orderInfoWindowDStream.join(orderDetailWindowDStream)
 
     // 去重(使用Redis进行去重)   key: order_join:[orderId]     value:orderDetailId        expire: 120S
@@ -103,7 +102,51 @@ object OrderWideApp {
         orderWideList.toIterator
       }
     }
-    orderWideDStream.print(100)
+    // =======================3.实付分摊=========================
+    val orderWideDetailDStream: DStream[OrderWide] = orderWideDStream.mapPartitions {
+      orderWideItr: Iterator[OrderWide] => {
+        val orderWideList: List[OrderWide] = orderWideItr.toList
+        // 获取Redis连接
+        val jedisClient: Jedis = MyRedisUtil.getJedisClient
+        for (orderWide <- orderWideList) {
+          // 从redis中获取明细累加和
+          val order_origin_sum_key: String = "order_origin_sum:" + orderWide.order_id
+          var order_origin_sum: Double = 0D
+          val orderOriginSum: String = jedisClient.get(order_origin_sum_key)
+          // Redis中获取字符串必须要进行非空判断
+          if (orderOriginSum != null && orderOriginSum.nonEmpty) {
+            order_origin_sum = orderOriginSum.toDouble
+          }
+          // 从Redis中获取实付分摊累加和
+          val order_detail_sum_key: String = "order_split_sum:" + orderWide.order_id
+          var order_detail_sum: Double = 0D
+          val orderDetailSum: String = jedisClient.get(order_detail_sum_key)
+          if (orderDetailSum != null && orderDetailSum.nonEmpty) {
+            order_detail_sum = orderDetailSum.toDouble
+          }
+
+          val OriginAmount: Double = orderWide.sku_price * orderWide.sku_num
+          // 判断是否最后一条明细并进行实付分摊
+          if (OriginAmount == orderWide.original_total_amount - order_origin_sum) {
+            // 最后一条明细
+            orderWide.final_detail_amount = Math.round((orderWide.final_total_amount - order_detail_sum) * 100D) / 100D
+          } else {
+            orderWide.final_detail_amount = orderWide.final_total_amount * OriginAmount / orderWide.original_total_amount
+          }
+          // 更新Redis中的值
+          val newOriginAmount: Double = order_origin_sum + OriginAmount
+          val newDetailSum: Double = order_detail_sum + orderWide.final_detail_amount
+          jedisClient.setex(order_origin_sum_key, 600, newOriginAmount.toString)
+          jedisClient.setex(order_detail_sum_key, 600, newDetailSum.toString)
+        }
+        jedisClient.close()
+        orderWideList.toIterator
+      }
+    }
+
+    orderWideDetailDStream.print(100)
+
+
 
     ssc.start()
     ssc.awaitTermination()
